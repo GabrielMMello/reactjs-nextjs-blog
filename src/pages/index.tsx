0a063edb47bd50e1f23a85df06b59dca7d18e6c0
Gabriel Mendes Mello <gabrielmendesmello@gmail.com>
2021-08-27 20:45:31 -0300

    Home layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai'
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,64 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(props: HomeProps) {
+  // TODO
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.subContainer}>
+              <div className={styles.item}>
+                <AiOutlineCalendar />
+                <time>12 de maio</time>
+              </div>
+              <div className={styles.item}>
+                <AiOutlineUser />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.subContainer}>
+              <div className={styles.item}>
+                <AiOutlineCalendar />
+                <time>12 de maio</time>
+              </div>
+              <div className={styles.item}>
+                <AiOutlineUser />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.subContainer}>
+              <div className={styles.item}>
+                <AiOutlineCalendar />
+                <time>12 de maio</time>
+              </div>
+              <div className={styles.item}>
+                <AiOutlineUser />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
